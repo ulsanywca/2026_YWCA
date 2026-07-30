6b74e307557702704a177648c9f9d62a06f83331
ulsanywca <ulywca@gmail.com>
2026-07-30 16:58:15 +0900

    Update sample-data.js

--- sample-data.js
+++ sample-data.js
@@ -3,9 +3,9 @@
  * 실제 운영 시에는 config.js를 google-sheet 모드로 전환하세요.
  */
 window.SAMPLE_GUESTS = [
-  { id: "sample-1", name: "김한별", region: "울산", room: "305", roommates: ["박연이", "이와이"] },
-  { id: "sample-2", name: "박연이", region: "울산", room: "305", roommates: ["김한별", "이와이"] },
-  { id: "sample-3", name: "이와이", region: "울산", room: "305", roommates: ["김한별", "박연이"] },
+  { id: "sample-1", name: "권한별", region: "울산", room: "305", roommates: ["박연이", "이와이"] },
+  { id: "sample-2", name: "박연이", region: "울산", room: "305", roommates: ["권한별", "이와이"] },
+  { id: "sample-3", name: "이와이", region: "울산", room: "305", roommates: ["권한별", "박연이"] },
   { id: "sample-4", name: "최고래", region: "울산", room: "407", roommates: ["정태화"] },
   { id: "sample-5", name: "정태화", region: "울산", room: "407", roommates: ["최고래"] },
   { id: "sample-6", name: "김은하", region: "부산", room: "208", roommates: ["오바다"] },
